0c91543871d6b8cd0cd9bbe98d9838aed781b3ce
Venkata Reddy M <venkatareddy.mulam@techmahindra.com>
2017-03-24 07:37:25 -0400

    Update models.py

--- models.py
+++ models.py
@@ -10,3 +10,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    
+class MyChoiceQuestion(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
